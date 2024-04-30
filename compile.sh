6b0ef2cf36638493688100dbec143ab76f6e356c
Bernd Kiefer <j_a_w_bone@gmx.de>
2024-04-30 10:01:25 +0200

    Improve compile.sh to only compile thrift if necessary

--- compile.sh
+++ compile.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 scriptdir=`dirname $0`
 
+findnewest() {
+    unset -v latest
+    for file in `find "$@" -type f`; do
+        test -z "$latest" -o "$file" -nt "$latest" && latest="$file"
+    done
+    echo "$latest"
+}
+
 cd "$scriptdir"
-./run-thrift
+newestgen=`findnewest src/main/gen*`
+newestthrift=`findnewest src/main/thrift`
+[[ "$newestthrift" -nt "$newestgen" ]] && ./run-thrift
+
 mvn -U clean install
 mvn -f apps.xml install
