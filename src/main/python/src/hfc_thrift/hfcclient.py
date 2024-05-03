9e4860da6e248ac88ec7cea0aa55fb007ce17254
Jörg Steffen <steffen@dfki.de>
2024-05-03 17:03:53 +0200

    Add more type hints

--- src/main/python/src/hfc_thrift/hfcclient.py
+++ src/main/python/src/hfc_thrift/hfcclient.py
@@ -7,7 +7,7 @@ from hfc_thrift.hfc_db import HfcDbService
 
 
 class HfcClient(HfcDbService.Client):
-    def __init__(self, hostname, port):
+    def __init__(self, hostname: str, port: int) -> None:
         # Make socket
         self.transport = TSocket.TSocket(hostname, port)
 
@@ -20,20 +20,20 @@ class HfcClient(HfcDbService.Client):
         # Create a client to use the protocol encoder
         super().__init__(protocol)
 
-    def connect(self):
+    def connect(self) -> None:
         # Connect!
         self.transport.open()
 
-    def ping(self):
+    def ping(self) -> None:
         super().ping()
         print('ping()')
 
     # Close!
-    def disconnect(self):
+    def disconnect(self) -> None:
         self.transport.close()
 
 
-def connect(host='localhost', port=9090) -> HfcClient:
+def connect(host: str = 'localhost', port: int = 9090) -> HfcClient:
     """
     classmapping is a map from class uri to (simple) name
     ns is the namespace where new instances are created
@@ -46,13 +46,13 @@ def connect(host='localhost', port=9090) -> HfcClient:
 
 # Only for testing
 # start the HFC server first:
-# bin/startServer src/test/data/test.yml 
+# bin/startServer src/test/data/test.yml
 if __name__ == '__main__':
     try:
         client = connect('localhost', 7777)
         qr = client.selectQuery('select ?uri where ?uri <rdf:type> <owl:Class> ?_')
-        print(qr.table.rows[1][0])
+        print(qr.table.rows[1][0])  # type: ignore
         client.disconnect()
 
     except Thrift.TException as tx:
-        print('%s' % tx.message)
+        print('%s' % tx.message)  # type: ignore
