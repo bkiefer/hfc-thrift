a8e0eb09bdf3e4d771fab3584ec7753e609ad37c
Jörg Steffen <steffen@dfki.de>
2024-05-24 15:20:25 +0100

    Reorganize Python code

--- src/main/python/src/hfc_thrift/hfcclient.py
+++ src/main/python/src/hfc_thrift/hfcclient.py
@@ -3,7 +3,7 @@ from thrift.protocol import TBinaryProtocol
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 
-from hfc_thrift.hfc_db import HfcDbService
+from hfc_db import HfcDbService
 
 
 class HfcClient(HfcDbService.Client):
